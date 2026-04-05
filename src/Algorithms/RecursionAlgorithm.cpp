77367fc76e40fb267009bb24b8267bf5b93b4fc5
Ellxander <202301138@iacademy.edu.ph>
2026-04-05 14:29:53 +0800

    Added Fibonacci & TowerOfHanoi Recursion

--- src/Algorithms/RecursionAlgorithm.cpp
+++ src/Algorithms/RecursionAlgorithm.cpp
@@ -1,4 +1,5 @@
 #include "RecursionAlgorithm.h"
+#include <iostream>
 
 int Factorial::Factorial(int n) {
     if (n <= 1) {
@@ -7,4 +8,23 @@ int Factorial::Factorial(int n) {
     int result = n * Factorial(n - 1);
     
     return result;
+}
+
+int RecursionAlgorithm::Fibonacci(int n){
+    if(n <= 1){
+        return n;
+    }
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+void RecursionAlgorithm::TowerOfHanoi(int numDisks, char source, char auxiliary, char destination){
+    if(numDisks <= 0){
+        return;
+    }
+
+    TowerOfHanoi(numDisks - 1, source, destination, auxiliary);
+    std::cout << "Move disk " << numDisks << " from " << source << " to " << destination << std::endl;
+
+    TowerOfHanoi(numDisks - 1, auxiliary, source, destination);
 }
